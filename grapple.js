cb39efe20c9a9191f194b986cce87cbf5ee494bb
Joshua Schecker <Joshua.Schecker@atkinsglobal.com>
2021-02-16 22:32:54 +0100

    silver carillon, ki strike

--- grapple.js
+++ grapple.js
@@ -2,39 +2,29 @@ const targets = Array.from(game.user.targets);
 if (targets.length != 1) {
   ui.notifications.error("Please target one token");
 }
-const mountainStance = actor.data.data.customModifiers.ac.some((mod) => mod.name === "Mountain Stance");
 new Dialog({
   title: `Grapple`,
   buttons: {
     first: {
       label: "1st attack",
-      callback: () => grapple(1),
+      callback: () => grapple(0),
     },
     second: {
       label: "2nd attack",
-      callback: () => grapple(2),
+      callback: () => grapple(5),
     },
     third: {
       label: "3rd attack",
-      callback: () => grapple(3),
+      callback: () => grapple(10),
     },
   },
   default: "first",
 }).render(true);
 
 function grapple(map) {
-  let mapMod;
-  switch (map) {
-    case 2:
-      mapMod = mountainStance ? 5 : 4;
-      break;
-    case 3:
-      mapMod = mountainStance ? 10 : 8;
-      break;
-  }
   let target = targets[0];
   const dc = target.actor.data.data.saves.fortitude.totalModifier + 10;
-  const roll = new Roll(`1d20 + ${actor.data.data.skills.ath.value} ${mapMod ? `- ${mapMod}` : ""}`).roll();
+  const roll = new Roll(`1d20 + ${actor.data.data.skills.ath.value} - ${map}`).roll();
   const difference = roll.total - dc;
   let degree = difference < 0 ? 0 : 1;
   if (roll.results[0] == 20 || difference >= 10) {
@@ -45,16 +35,16 @@ function grapple(map) {
   let content;
   switch (degree) {
     case -1:
-      content = `<h3>Grapple <span style="color: red;">❌❌CRITICAL Fail</span></h3></p>${actor.data.name} is <b>Grabbed</b> or knocked <b>Prone</b></p>`;
+      content = `<h3>Grapple <span style="color: red;">❌❌CRITICAL Fail</span></h3></p>${actor.data.name} is <b>Grampendium[pf2e.conditionitems.kWc1fhmv9LBiTuei]{Grabbed} or knocked <b>@Compendium[pf2e.conditionitems.kWc1fhmv9LBiTuei]{Prone}</p>`;
       break;
     case 0:
       content = '<h3>Grapple <span style="color: grey;">❌Fail</span></h3>';
       break;
     case 1:
-      content = `<h3>Grapple <span style="color: green;">✔️Success</span></h3></p>${target.actor.data.name} is <b>Grabbed</b></p>`;
+      content = `<h3>Grapple <span style="color: green;">✔️Success</span></h3></p>${target.actor.data.name} is <b>@Compendium[pf2e.conditionitems.kWc1fhmv9LBiTuei]{Grabbed}</b></p>`;
       break;
     case 2:
-      content = `<h3>Grapple <span style="color: green;">✔️✔️CRITICAL Success</span></h3></p>${target.actor.data.name} is <b>Restrained</b></p>`;
+      content = `<h3>Grapple <span style="color: green;">✔️✔️CRITICAL Success</span></h3></p>${target.actor.data.name} is <b>$Compendium[pf2e.conditionitems.kWc1fhmv9LBiTuei]{Restrained}}</b></p>`;
       break;
   }
   let chatData = {
@@ -62,5 +52,10 @@ function grapple(map) {
     speaker: ChatMessage.getSpeaker(),
   };
   ChatMessage.create({ ...chatData, roll, type: CHAT_MESSAGE_TYPES.ROLL }, {});
+  if (degree > 0) {
+    const damageRoll = new Roll(actor.data.data.abilities.str.mod.toString()).roll();
+    console.log(damageRoll);
+    ChatMessage.create({ ...chatData, roll: damageRoll, type: CHAT_MESSAGE_TYPES.ROLL }, {});
+  }
   ChatMessage.create({ ...chatData, content }, {});
 }
